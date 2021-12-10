b12da40e3257ac59ab089e5fe23dc319ac1cd5ec
larmes <57335121+larmes@users.noreply.github.com>
2021-12-10 00:01:53 -0600

    redone analog!

---
+++ main.js
@@ -0,0 +1,40 @@
+// For toggle button;
+
+function change()
+{
+
+    const body = document.querySelector('body');
+    body.classList.toggle('light');
+    body.style.transition = `0.3s linear`;
+}
+
+// for time;
+const deg = 6; 
+// 360 / (12 * 5);
+
+const hr = document.getElementById('hours');
+const mn = document.getElementById('minutes');
+const sc = document.getElementById('seconds');
+
+
+
+setInterval(() => {
+    
+    let day = new Date();
+    let hh = day.getHours() * 30;
+    let mm = day.getMinutes() * deg;
+    let ss = day.getSeconds() * deg;
+    let msec = day.getMilliseconds();
+    
+
+    
+    hr.style.transform = `rotateZ(${(hh) + (mm / 12)}deg)`;
+    mn.style.transform = `rotateZ(${mm}deg)`;
+    sc.style.transform = `rotateZ(${ss}deg)`;
+    
+    // gives the smooth transitioning effect, but there's a bug here!
+    // sc.style.transition = `1s`;
+    
+
+})
+
